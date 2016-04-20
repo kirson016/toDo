705206c380b3409a7db43b09c5e7f5848a4159a1
Kirson <adam.lubanskii@gmail.com>
2016-04-20 15:22:38 +0200

    System checking input value with alert

--- JS/javaScript.js
+++ JS/javaScript.js
@@ -7,6 +7,8 @@
         elAddButton: document.getElementById('btn-AddNewTask'),
         elResponceSpan: document.getElementById('responce'),
         elTextInput: document.getElementById("taskName"),
+        elTaskName: document.getElementById("taskName"),
+        elTaskPriority: document.getElementById("TaskPriority"),
         elValue0: document.getElementById("value0"),
         elValue1: document.getElementById("value1"),
         elValue3: document.getElementById("value3"),
@@ -15,10 +17,17 @@
             var priority;
             var z = boxName[number];
             var text = Tasks.elTextInput.value;
+            var TaskPriority = Tasks.elTaskPriority.value;
+            if (text.length<3 || TaskPriority == "none")
+            {
+              alert("Task name is to short! or didn't set Priority");
+            }
+            else{
             Tasks.elResponceSpan.innerHTML += '<input type="checkbox" aria-label="...">     ' + text + '      '  ;
             countBox += 1;
             number += 1;
-            alert(text);
+            alert(TaskPriority);
+          };
         },
     };
     Tasks.elAddButton.onclick = Tasks.resetInput;
