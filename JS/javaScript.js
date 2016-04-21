e365e32604e094078a99e713c84aa01764daef7a
Kirson <adam.lubanskii@gmail.com>
2016-04-21 10:46:27 +0200

    good working add tasks with good looking

--- JS/javaScript.js
+++ JS/javaScript.js
@@ -19,21 +19,18 @@
             var text = Tasks.elTextInput.value;
             var TaskPriority = Tasks.elTaskPriority.value;
 
-            if (TaskPriority == 3)
-            {
-              priority= '<button type="button" class="btn btn-success">LOW</button>';
-            }
-            else if (TaskPriority == 1) {
-                priority= '<button type="button" class="btn btn-warning">MEDIUM</button>';
-            }
-            else {
-              priority= '<button type="button" class="btn btn-danger">HIGH</button>';
+            if (TaskPriority == 3) {
+                priority = '<button type="button" class="btn-group-sm btn-success " id="priorityButtons">LOW</button>>';
+            } else if (TaskPriority == 1) {
+                priority = '<button type="button" class="btn-group-sm btn-warning " id="priorityButtons">MEDIUM</button>';
+            } else {
+                priority = '<button type="button" class="btn-group-sm btn-danger " id="priorityButtons">HIGH</button>';
             }
 
             if (text.length < 3 || TaskPriority == "none") {
                 alert("Task name is to short! or didn't set Task Priority");
             } else {
-                Tasks.elResponceSpan.innerHTML += '<p class="userTaskText"> '+text+ '</p><input type="checkbox" id="checkBox" aria-label="..."> '+priority ;
+                Tasks.elResponceSpan.innerHTML += '<div class="taskSelector"><p class="userTaskText"> ' + text + '</p><input type="checkbox" id="checkBox" aria-label="..."> ' + priority + '<div class="taskSelector"> ';
                 countBox += 1;
                 number += 1;
                 //alert(priority);
