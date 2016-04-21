a7385b3aeff757833658b4f98d7b18b552e3ce18
Kirson <adam.lubanskii@gmail.com>
2016-04-21 11:36:27 +0200

    windows possition swaped

--- JS/javaScript.js
+++ JS/javaScript.js
@@ -5,6 +5,7 @@
 
     var Tasks = {
         elAddButton: document.getElementById('btn-AddNewTask'),
+        elDeleteTask: document.getElementById('btnClearTaskSize'),
         elResponceSpan: document.getElementById('responce'),
         elTextInput: document.getElementById("taskName"),
         elTaskName: document.getElementById("taskName"),
@@ -20,7 +21,7 @@
             var TaskPriority = Tasks.elTaskPriority.value;
 
             if (TaskPriority == 3) {
-                priority = '<button type="button" class="btn-group-sm btn-success " id="priorityButtons">LOW</button>>';
+                priority = '<button type="button" class="btn-group-sm btn-success " id="priorityButtons">LOW</button>';
             } else if (TaskPriority == 1) {
                 priority = '<button type="button" class="btn-group-sm btn-warning " id="priorityButtons">MEDIUM</button>';
             } else {
@@ -30,11 +31,24 @@
             if (text.length < 3 || TaskPriority == "none") {
                 alert("Task name is to short! or didn't set Task Priority");
             } else {
-                Tasks.elResponceSpan.innerHTML += '<div class="taskSelector"><p class="userTaskText"> ' + text + '</p><input type="checkbox" id="checkBox" aria-label="..."> ' + priority + '<div class="taskSelector"> ';
+                Tasks.elResponceSpan.innerHTML += '<div class="checkboxid"><p class="userTaskText"> ' + text + '</p><input type="checkbox" id="checkBox" class="checked" aria-label="..."> ' + priority ;
                 countBox += 1;
                 number += 1;
                 //alert(priority);
             }
         },
+      deleteCheckBox:  function (){
+        var boxes = document.getElementsByClassName('checked');
+        var texts = document.getElementsByClassName('checkboxid');
+        for(var i = 0; i<boxes.length; i++){
+            box = boxes[i];
+            txt = texts[i];
+            if(box.checked){
+                box.parentNode.removeChild(box);
+                txt.parentNode.removeChild(txt);
+            }
+        }
+    }
     };
     Tasks.elAddButton.onclick = Tasks.resetInput;
+    Tasks.elDeleteTask.onclick = Tasks.deleteCheckBox;
